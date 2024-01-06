0d7e30854094518abf4eec7948413f521c262c70
Zohair Oomatia <zohairoomatia@Zohairs-MacBook-Pro.local>
2024-01-06 18:44:30 +1100

    simple routes created for tasks and placed in their respective folders

--- starter/routes/tasks.js
+++ starter/routes/tasks.js
@@ -1,8 +1,20 @@
-const express = require('express');
+// This file keeps all the routes relating to tasks and is connected with the controllers
+// folder to use its functions 
+const express = require('express')
 const router = express.Router();
 
-router.route('/').get((req,res) => {
-    res.send('all items');
-})
+const {
+    getAllTasks, 
+    getTask, 
+    updateTask, 
+    createTask, 
+    deleteTask
+} = require('../controllers/tasks')
+
+
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+
+
 
 module.exports = router;
